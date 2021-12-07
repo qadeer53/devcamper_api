d2965a3a92a5e193ba2cd134c104457459a65fd2
Muhammad Qadeer <71921233+qadeer53@users.noreply.github.com>
2021-12-07 13:59:10 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config( { path: 'config/config.env' })
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
